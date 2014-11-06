a7573659e1e43256d97649b72975ff59f0dfed68
mdifferent <mdifferent@163.com>
2014-11-06 18:17:01 +0800

    CharLayer begin

--- Views/CharacterLayer.cpp
+++ Views/CharacterLayer.cpp
@@ -10,25 +10,60 @@
 
 bool CharacterLayer::init()
 {
+	if (!Layer::init())
+		return false;
+
+	Size winSize = Director::getInstance()->getWinSize();
+	_leftPosX = winSize.width * 0.25;
+	_centerPosX = winSize.width * 0.5;
+	_rightPosX = winSize.width * 0.75;
+
+	_left = Sprite::create();
+	_left->setPosition(Vec2(_leftPosX, 0));
+	_left->setAnchorPoint(Vec2(0.5, 0));
+	_left->setOpacity(0);
+	_center = Sprite::create();
+	_center->setPosition(Vec2(_centerPosX, 0));
+	_center->setAnchorPoint(Vec2(0.5, 0));
+	_center->setOpacity(0);
+	_right = Sprite::create();
+	_right->setPosition(Vec2(_rightPosX, 0));
+	_right->setAnchorPoint(Vec2(0.5, 0));
+	_right->setOpacity(0);
+
     return true;
 }
 
-void CharacterLayer::setCharacter(const string& name, const int position, Vector<string> *property, ActionInterval *action)
+void CharacterLayer::showCharacter(const string& name, const CHAR_POSITION position, Vector<string> property, CHAR_TRANSITION tran)
+{
+	string path;
+	path.append(name);
+	for (string prop : property) {
+		path.append("_");
+		path.append(prop);
+	}
+
+	switch (position)
+	{
+	case CHAR_POSITION::LEFT:
+		_left->setSpriteFrame(path);
+		break;
+	default:
+		break;
+	}
+}
+
+void CharacterLayer::showCharacter(const string& name, Vec2 posistion, Vector<string> *property, CHAR_TRANSITION tran)
 {
     
 }
 
-void CharacterLayer::setCharacter(const string& name, Vec2 posistion, Vector<string> *property, ActionInterval *action)
+void CharacterLayer::showCharacter(const string& name, float xpos, Vector<string> *property, CHAR_TRANSITION tran)
 {
     
 }
 
-void CharacterLayer::setCharacter(const string& name, float xpos, Vector<string> *property, ActionInterval *action)
-{
-    
-}
-
-void CharacterLayer::removeCharacter(const string& name, ActionInterval *action)
+void CharacterLayer::removeCharacter(const string& name, CHAR_TRANSITION tran)
 {
     
 }
--- Views/CharacterLayer.h
+++ Views/CharacterLayer.h
@@ -16,20 +16,42 @@
 USING_NS_CC;
 using namespace std;
 
+enum class CHAR_POSITION {
+	LEFT,
+	CENTER,
+	RIGHT
+};
+
+enum class CHAR_TRANSITION {
+	DISSOLVE,
+	MOVE_LEFT,
+	MOVE_RIGHT,
+	MOVE_BOTTOM,
+	MOVE_TOP,
+};
+
 class CharacterLayer : public Layer
 {
 public:
     virtual bool init();
     CREATE_FUNC(CharacterLayer);
     
-    void setCharacter(const string& name, const int position, Vector<string> *property = NULL, ActionInterval *action = NULL);
-    void setCharacter(const string& name, Vec2 posistion, Vector<string> *property = NULL, ActionInterval *action = NULL);
-    void setCharacter(const string& name, float xpos, Vector<string> *property = NULL, ActionInterval *action = NULL);
-    void removeCharacter(const string& name, ActionInterval *action = NULL );
+	void showCharacter(const string& name, const CHAR_POSITION position, Vector<string> property = NULL, CHAR_TRANSITION trans = CHAR_TRANSITION::DISSOLVE);
+	void showCharacter(const string& name, Vec2 posistion, Vector<string> *property = NULL, CHAR_TRANSITION trans = CHAR_TRANSITION::DISSOLVE);
+	void showCharacter(const string& name, float xpos, Vector<string> *property = NULL, CHAR_TRANSITION trans = CHAR_TRANSITION::DISSOLVE);
+	void removeCharacter(const string& name, CHAR_TRANSITION trans = CHAR_TRANSITION::DISSOLVE);
     int getCharacterCount();
+
+	CC_SYNTHESIZE(float, _leftPosX, LeftPosX);
+	CC_SYNTHESIZE(float, _centerPosX, CenterPosX);
+	CC_SYNTHESIZE(float, _rightPosX, RightPosX);
     
 private:
     map<string, Vec2> _characters;
+
+	Sprite *_left;
+	Sprite *_center;
+	Sprite *_right;
 };
 
 #endif
